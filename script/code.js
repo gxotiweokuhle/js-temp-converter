7afdf3ac9e8cf763e1cf9f4186990cbb34afaad0
okuhle gxotiwe <gxotiweokuhle@gmail.com>
2023-05-25 09:32:49 +0200

    done

--- script/code.js
+++ script/code.js
@@ -5,6 +5,15 @@ fahrenheit.oninput = function () {
         fahrenheit.value) - 32) * 5) / 9;
     celsius.value = parseFloat(c.toFixed(2));
 }
+
+clearbutton = document.getElementById('reset');
+clearbutton.addEventListener('click', function(){
+    //console.log('hey it works')
+    fahrenheit.value = "";
+    celsius.value = "";
+   
+})
+
 // allclear = document.getElementById('cel');
 //let empty = document.getElementById('fah');
 
